ea5f6af4f6ad9180c586c01c4cd526eb05f34f42
Lucas Rodrigues Góes <lucas.rodrigues.goes@gmail.com>
2020-02-17 21:02:44 -0300

    Plate Creation

---
+++ Assets/_ink/Plate/ControlBar.cs
@@ -0,0 +1,19 @@
+﻿using ScriptableObjectArchitecture;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlBar : MonoBehaviour
+{
+    public FloatVariable eatenPercentage;
+    private Image image;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();    
+    }
+
+    private void FixedUpdate()
+    {
+        image.fillAmount = eatenPercentage.Value/100f;
+    }
+}
---
+++ Assets/_ink/Plate/PlateClickControl.cs
@@ -0,0 +1,12 @@
+﻿using ScriptableObjectArchitecture;
+using UnityEngine;
+
+public class PlateClickControl : MonoBehaviour
+{
+    public FloatVariable eatenPercentage;
+
+    private void OnMouseOver()
+    {
+        eatenPercentage.Value += 0.1f;
+    }
+}
