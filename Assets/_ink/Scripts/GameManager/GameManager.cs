f9fccd0af8df8d10f658c432b3d9e5bd8d6fc633
Lucas Rodrigues Góes <lucas.rodrigues.goes@gmail.com>
2020-03-02 21:54:01 -0300

    GameManagar

--- Assets/_ink/Scripts/GameManager/GameManager.cs
+++ Assets/_ink/Scripts/GameManager/GameManager.cs
@@ -1,13 +1,26 @@
 ﻿using UnityEngine;
 using ScriptableObjectArchitecture;
 
-    public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    
+    public IntVariable foodScore;
+    public int foodScoreValue;
 
+    public FloatVariable initialTimer;
+    public float initialTimeValue;
+
+    public GameEvent gameStartedEvent;
+
+    private void Awake()
+    {
+        InitGame();
+    }
 
     void InitGame()
     {
+        foodScore.Value = foodScoreValue;
+        initialTimer.Value = initialTimeValue;
 
+        gameStartedEvent.Raise();
     }
 }
