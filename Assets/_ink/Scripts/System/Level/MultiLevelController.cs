2c11e0578f7d12fb40ae59a326032a46dee96af6
Lucas Rodrigues Góes <lucas.rodrigues.goes@gmail.com>
2020-03-29 00:40:33 -0300

    making modular scene

---
+++ Assets/_ink/Scripts/System/Level/MultiLevelController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "ModuleController", menuName = "Managers/Modules", order = 1)]
+public class MultiLevelController : ScriptableObject
+{
+    public List<Scene> levels;
+    public Scene currentLevel;
+
+    public void NextLevel()
+    {
+        SceneManager.SetActiveScene(currentLevel);
+    }
+
+}
