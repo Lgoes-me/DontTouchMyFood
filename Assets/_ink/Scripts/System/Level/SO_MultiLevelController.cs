4607cf087da077bad84a72ab223d282171589e47
Lucas Rodrigues Góes <lucas.rodrigues.goes@gmail.com>
2020-03-30 03:39:40 -0300

    making modular levels

---
+++ Assets/_ink/Scripts/System/Level/SO_MultiLevelController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "ModuleController", menuName = "Managers/Modules", order = 1)]
+public class SO_MultiLevelController : ScriptableObject
+{
+    public string [] levelNames;
+    public int currentLevel = 0;
+    
+    public void Init()
+    {
+        currentLevel = 0;
+        SceneManager.LoadSceneAsync(levelNames[currentLevel], LoadSceneMode.Additive);
+    }
+
+    public void NextLevel()
+    {
+        if(currentLevel < levelNames.Length - 1)
+        {
+            SceneManager.UnloadSceneAsync(levelNames[currentLevel]);
+            currentLevel += 1;
+            SceneManager.LoadSceneAsync(levelNames[currentLevel], LoadSceneMode.Additive);
+        }
+    }
+}
