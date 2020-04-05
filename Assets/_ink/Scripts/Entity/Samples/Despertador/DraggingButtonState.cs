1f0c75b3e8146b1035997509a35d70554fd4302d
Lucas Rodrigues Góes <lucas.rodrigues.goes@gmail.com>
2020-04-05 00:46:15 -0300

    finishing first scene

---
+++ Assets/_ink/Scripts/Entity/Samples/Despertador/DraggingButtonState.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ink.DontTouchMyFood.Entity;
+using ScriptableObjectArchitecture;
+
+public class DraggingButtonState : EntityState
+{
+    public Vector3Variable inputPosition;
+    public BoolGameEvent endScene;
+    private Vector3 offset;
+
+    public float waitTime;
+    public float speed;
+
+    private bool _touched;
+
+    public override void OnStateInputReceived(bool touch)
+    {
+        base.OnStateInputReceived(touch);
+
+        _touched = touch;
+        offset = inputPosition.Value - transform.localPosition;
+        StartCoroutine(EndScene());
+    }
+
+
+    public override void OnStateUpdate()
+    {
+        if (_touched)
+        {
+            _rigidbody2D.position = Vector3.Lerp(transform.position, new Vector3(inputPosition.Value.x - offset.x, inputPosition.Value.y - offset.y, inputPosition.Value.z), speed);
+        }
+    }
+
+    private IEnumerator EndScene()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        endScene.Raise(true);
+    }
+}
---
+++ Assets/_ink/Scripts/Entity/Samples/Despertador/IdleState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ink.DontTouchMyFood.Entity;
+
+public class IdleState : EntityState
+{
+    public override void OnStateInputReceived(bool input)
+    {
+        base.OnStateInputReceived(input);
+
+        if (input)
+        {
+            _rigidbody2D.velocity = Vector2.zero;
+            _controller.SetState(this.GetComponent<DraggingButtonState>());
+        }
+    }
+}
