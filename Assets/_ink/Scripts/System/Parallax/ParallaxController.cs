9ce49c182b454aee363b5341d8df7dc04ee1e01f
Lucas <lucas.rodrigues.goes@gmail.com>
2020-04-19 21:07:17 -0300

    parallax effects and some other changes

---
+++ Assets/_ink/Scripts/System/Parallax/ParallaxController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using ScriptableObjectArchitecture;
+
+public class ParallaxController : MonoBehaviour
+{
+    public QuaternionVariable gyroInput;
+    public Vector3Variable acelerometerInput;
+
+    private Gyroscope _gyroscope;
+    
+    void Start()
+    {
+        _gyroscope = Input.gyro;
+        _gyroscope.enabled = true;
+    }
+    
+    void Update()
+    {
+        gyroInput.Value = _gyroscope.attitude;
+        acelerometerInput.Value = Input.acceleration;
+    }
+}
---
+++ Assets/_ink/Scripts/System/Parallax/ParallaxObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using ScriptableObjectArchitecture;
+
+public class ParallaxObject : MonoBehaviour
+{
+    public QuaternionVariable gyroQuaternion;
+    public Vector3Variable acelInput;
+    public float objectIndex;
+    private Vector3 _initialPosition;
+
+    private void Start()
+    {
+        _initialPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float xTranslation = -(gyroQuaternion.Value.eulerAngles.x  + acelInput.Value.x) * objectIndex;
+        float yTranslation = -(gyroQuaternion.Value.eulerAngles.y + acelInput.Value.y) * objectIndex;
+
+        Vector3 translation = new Vector3(xTranslation, yTranslation, 0);
+
+        transform.position = _initialPosition + translation;
+    }
+}
