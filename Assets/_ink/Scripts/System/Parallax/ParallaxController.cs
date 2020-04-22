acdbed149768513b220de6ab7b2a9e5e75332179
Lucas <lucas.rodrigues.goes@gmail.com>
2020-04-21 21:51:48 -0300

    fixing parallax

--- Assets/_ink/Scripts/System/Parallax/ParallaxController.cs
+++ Assets/_ink/Scripts/System/Parallax/ParallaxController.cs
@@ -7,19 +7,36 @@ public class ParallaxController : MonoBehaviour
     public Vector3Variable acelerometerInput;
 
     private Gyroscope _gyroscope;
-    private Vector3 _initialOrientation;
-    
+
+    private bool _useGyro = false;
+
+    Matrix4x4 baseMatrix = Matrix4x4.identity;
+
+    public void Calibrate()
+    {
+        Quaternion rotate = Quaternion.FromToRotation(new Vector3(0.0f, 0.0f, -1.0f), Input.acceleration);
+
+        Matrix4x4 matrix = Matrix4x4.TRS(Vector3.zero, rotate, new Vector3(1.0f, 1.0f, 1.0f));
+
+        this.baseMatrix = matrix.inverse;
+    }
+
     void Start()
     {
+        _useGyro = SystemInfo.supportsGyroscope;
+
         _gyroscope = Input.gyro;
         _gyroscope.enabled = true;
-        _initialOrientation = Input.acceleration;
+
+        Calibrate();
 
     }
     
     void Update()
     {
-        gyroInput.Value = _gyroscope.attitude;
-        acelerometerInput.Value = Input.acceleration - _initialOrientation;
+        if(_useGyro)
+            gyroInput.Value = _gyroscope.attitude;
+        else
+            acelerometerInput.Value = Vector3.ClampMagnitude(acelerometerInput.Value + baseMatrix.MultiplyVector(Input.acceleration), 1f);
     }
 }
--- Assets/_ink/Scripts/System/Parallax/ParallaxObject.cs
+++ Assets/_ink/Scripts/System/Parallax/ParallaxObject.cs
@@ -6,20 +6,28 @@ public class ParallaxObject : MonoBehaviour
     public QuaternionVariable gyroQuaternion;
     public Vector3Variable acelInput;
     public float objectIndex;
-    private Vector3 _initialPosition;
+
+    private Vector3 _realPosition;
+    private Vector3 _translation;
 
     private void Start()
     {
-        _initialPosition = transform.position;
+        _realPosition = transform.position;
+        _translation = Vector3.zero;
     }
 
-    void Update()
+    private void Update()
+    {
+        _realPosition = transform.position - _translation;
+    }
+
+    private void LateUpdate()
     {
         float xTranslation = - (gyroQuaternion.Value.eulerAngles.x  + acelInput.Value.x) * objectIndex;
         float yTranslation = - (gyroQuaternion.Value.eulerAngles.y + acelInput.Value.y) * objectIndex;
 
-        Vector3 translation = new Vector3(xTranslation, yTranslation, 0);
+        _translation = new Vector3(xTranslation, yTranslation, 0);
 
-        transform.position = Vector3.Lerp(transform.position, _initialPosition + translation, 30 * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, _realPosition + _translation, 30 * Time.deltaTime);
     }
 }
