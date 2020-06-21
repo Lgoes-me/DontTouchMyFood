28035304e1c09617e8eb47ffb82140a5d9f36538
Lucas <lucas.rodrigues.goes@gmail.com>
2020-06-21 04:30:29 -0300

    sistema de cores , menu principal com animação e logo, completando nova cena principal

--- Assets/_ink/Entities/_Base/Programming/Scripts/ColorColtroller.cs
+++ Assets/_ink/Entities/_Base/Programming/Scripts/ColorColtroller.cs
@@ -7,17 +7,26 @@ public class ColorColtroller : MonoBehaviour
     public LevelColor[] levelPresetColor;
     public GameObject[] gameObjects;
 
-    private void Awake()
-    {
-        int colorIndex = Random.Range(0, levelPresetColor.Length);
+    public int colorIndex = 0;
 
-        for(int i = 0; i < gameObjects.Length; i++)
+    [ContextMenu("ChangeToRandomColor")]
+    public void ChangeToRandomColor()
+    {
+        colorIndex = Random.Range(0, levelPresetColor.Length);
+        ChangeColor();
+    }
+
+    [ContextMenu("ChangeColor")]
+    public void ChangeColor()
+    {
+        for (int i = 0; i < gameObjects.Length; i++)
         {
             if (gameObjects[i].TryGetComponent<SpriteRenderer>(out SpriteRenderer spriteRenderer))
             {
                 spriteRenderer.color = levelPresetColor[colorIndex].colors[i];
 
-            } else if(gameObjects[i].TryGetComponent<Image>(out Image image))
+            }
+            else if (gameObjects[i].TryGetComponent<Image>(out Image image))
             {
                 image.color = levelPresetColor[colorIndex].colors[i];
             }
