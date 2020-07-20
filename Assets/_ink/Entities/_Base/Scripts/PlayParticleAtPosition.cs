c33fc209b7909532ee6937dee2dce019cc406560
Lucas <lucas.rodrigues.goes@gmail.com>
2020-07-20 00:23:05 -0300

    sound and logo

---
+++ Assets/_ink/Entities/_Base/Scripts/PlayParticleAtPosition.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using ScriptableObjectArchitecture;
+
+public class PlayParticleAtPosition : MonoBehaviour
+{
+    public Vector3Variable position;
+    public ParticleSystem particleSystem;
+
+    public void Play()
+    {
+        particleSystem.transform.position = new Vector3(position.Value.x, position.Value.y, transform.position.z);
+        particleSystem.Play();
+    }
+}
